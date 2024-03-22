ee75aa0d85cb1f797b12def7aa56b0285ea7299a
Jan Gross <jan.gross@regiojet.cz>
2024-03-22 15:23:22 +0100

    some tests

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const userName = document.getElementById('userName');
-const email = document.getElementById('email');
+const userNameInput = document.getElementById('userName');
+/* const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 const form = document.getElementById('form');
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById('submit'); */
+const message = 'Username is required';
+
+const showError = (status) => {
+  const formInput = userNameInput.parentElement;
+  if(status){
+    formInput.classList.add("error");
+  } else {
+    formInput.classList.add("success");
+  }
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  checkUserName(userName.value);
+  //showError(userName);
+  checkLength(userName)
 })
 
-const checkUserName = (userName) => {
-  if (userName === "") {
-    showError(userName, "Username cannot be empty");
-  } else if (userName.length < 3) {
-    showError(userName, "Username must be at least 3 characters");
-  }
+const checkLength = (userName) => {
+  userName.value.length < 3 ? showError(true) : showError(false); 
 }
+
+let number = 3
+const newArray = [{
+  id: 1,
+  name: "testing"
+},{
+  id: 2,
+  name: "fucking"
+},{
+  id: 3,
+  name: "hacking"
+}]
+
+const findData = newArray.find((idecko) => idecko.id == number)
+const result = findData.name = "updated"
+
+console.log(newArray);
+
+
